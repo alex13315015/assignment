cf10a352b9d6baf02b3501d95dd7d21a508bb202
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-07 11:25:56 +0900

    feat: Exception 생성 후 적용

---
+++ src/main/java/com/ryan9025/jpa/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ryan9025.jpa.exception;
+
+public class DataNotFoundException extends RuntimeException{
+        public DataNotFoundException(String message) {
+            super(message);
+        }
+    }
+
