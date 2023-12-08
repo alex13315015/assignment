f11a2593209d164ef0f80a05915a1b8913f4ee08
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-08 15:02:31 +0900

    feat: 회원가입을 위한 dto 생성

---
+++ src/main/java/com/ryan9025/myhomepage/dto/JoinDto.java
@@ -0,0 +1,28 @@
+package com.ryan9025.myhomepage.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JoinDto {
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String userID;
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String password;
+
+    private String name;
+
+    @Email
+    private String email;
+
+
+}
