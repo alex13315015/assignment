23fdb83760f4b7d9c53de5dbe65620e66ad11e30
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-08 09:40:03 +0900

    feat: SocialUserInfo 인터페이스 구현

---
+++ src/main/java/com/ryan9025/jpa/social/SocialUserInfo.java
@@ -0,0 +1,9 @@
+package com.ryan9025.jpa.social;
+
+public interface SocialUserInfo {
+
+    public String getProvider();
+    public String getProviderID();
+    public String getEmail();
+    public String getName();
+}
