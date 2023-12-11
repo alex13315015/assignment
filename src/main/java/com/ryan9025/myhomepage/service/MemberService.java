1065098dc62e0a918590e0571205cfa0058db1bc
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-11 14:55:04 +0900

    feat: modifyProcess 구현

--- src/main/java/com/ryan9025/myhomepage/service/MemberService.java
+++ src/main/java/com/ryan9025/myhomepage/service/MemberService.java
@@ -2,12 +2,17 @@ package com.ryan9025.myhomepage.service;
 
 import com.ryan9025.myhomepage.constant.Role;
 import com.ryan9025.myhomepage.dto.JoinDto;
+import com.ryan9025.myhomepage.dto.UpdateMemberDto;
 import com.ryan9025.myhomepage.entity.Member;
 import com.ryan9025.myhomepage.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -15,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Transactional
     public Member join(JoinDto joinDto) {
 
         Member dbJoinMember = Member.builder()
@@ -25,5 +31,19 @@ public class MemberService {
                 .name(joinDto.getName())
                 .build();
         return memberRepository.save(dbJoinMember);
+    }
+    @Transactional
+    public void updateMember(int id, UpdateMemberDto updateMemberDto) {
+        Optional<Member> findMember = memberRepository.findById(id);
+        if(findMember.isPresent()) {
+            Member member = findMember.get();
+            member.setName(updateMemberDto.getName());
+            member.setEmail(updateMemberDto.getEmail());
+            member.setPhoneNumber(updateMemberDto.getPhoneNumber());
+            member.setMbti(updateMemberDto.getMbti());
+            member.setDescription(updateMemberDto.getDescription());
+        } else {
+            throw new UsernameNotFoundException("등록되지 않은 회원입니다.");
+        }
     }
 }
