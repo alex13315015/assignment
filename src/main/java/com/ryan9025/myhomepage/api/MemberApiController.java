6ecbf478582baf3903f1ccee5de3158d4679f80e
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-11 14:53:58 +0900

    feat: profileImage update 컨트롤러

---
+++ src/main/java/com/ryan9025/myhomepage/api/MemberApiController.java
@@ -0,0 +1,26 @@
+package com.ryan9025.myhomepage.api;
+
+import com.ryan9025.myhomepage.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PutMapping("/api/member/{id}/profileImageUrl")
+    public Map<String,Object> profileImageUpdate(@PathVariable int id, MultipartFile profileImage) {
+        Map<String,Object> resultMap = new HashMap<>();
+        resultMap.put("isState","OK");
+        return resultMap;
+    }
+}
