29cf96e60ee300017c78b95cc88b5615e7d57d09
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-11 11:33:10 +0900

    feat: myPage 설정

--- src/main/java/com/ryan9025/myhomepage/controller/MemberController.java
+++ src/main/java/com/ryan9025/myhomepage/controller/MemberController.java
@@ -1,4 +1,21 @@
 package com.ryan9025.myhomepage.controller;
 
+import com.ryan9025.myhomepage.dto.CustomUserDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/member")
 public class MemberController {
+    @GetMapping("/myPage/{id}")
+    public String myPage(@PathVariable int id, Model model , @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        model.addAttribute("memberInfo",customUserDetails.getLoggedMember());
+        return "/member/myPage";
+    }
 }
