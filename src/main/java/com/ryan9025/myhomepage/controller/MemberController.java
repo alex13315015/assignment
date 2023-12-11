71c442177ce58c469feca03bdade9bb2b0ae3aa3
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-11 14:54:41 +0900

    feat: modifyProcess 구현

--- src/main/java/com/ryan9025/myhomepage/controller/MemberController.java
+++ src/main/java/com/ryan9025/myhomepage/controller/MemberController.java
@@ -1,18 +1,24 @@
 package com.ryan9025.myhomepage.controller;
 
 import com.ryan9025.myhomepage.dto.CustomUserDetails;
+import com.ryan9025.myhomepage.dto.UpdateMemberDto;
+import com.ryan9025.myhomepage.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
     @GetMapping("/myPage/{id}")
     public String myPage(@PathVariable int id, Model model , @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         model.addAttribute("memberInfo",customUserDetails.getLoggedMember());
@@ -23,5 +29,11 @@ public class MemberController {
     public String modify(@PathVariable int id, Model model , @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         model.addAttribute("memberInfo",customUserDetails.getLoggedMember());
         return "/member/modify";
+    }
+
+    @PostMapping("/modify/{id}")
+    public String modifyProcess(@PathVariable int id, Model model, UpdateMemberDto updateMemberDto) {
+        memberService.updateMember(id,updateMemberDto);
+        return "redirect:/member/myPage/" + id;
     }
 }
