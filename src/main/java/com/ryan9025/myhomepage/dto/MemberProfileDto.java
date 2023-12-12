fc98bff959227ac6e93d62ca026a9354c5c1a929
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-12 14:15:25 +0900

    feat: profile 관련 dto 생성

---
+++ src/main/java/com/ryan9025/myhomepage/dto/MemberProfileDto.java
@@ -0,0 +1,15 @@
+package com.ryan9025.myhomepage.dto;
+
+import com.ryan9025.myhomepage.entity.Member;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberProfileDto {
+    private boolean pageOwner;
+    private Member member;
+    private int subscribeCount;
+}
