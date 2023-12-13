925e54d57dcb13a0c3167d9e71aebff30e4475da
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-13 15:33:56 +0900

    feat: 구독자 확인 기능 추가 및 구독 관련 기능 수정

--- src/main/java/com/ryan9025/myhomepage/api/MemberApiController.java
+++ src/main/java/com/ryan9025/myhomepage/api/MemberApiController.java
@@ -1,24 +1,29 @@
 package com.ryan9025.myhomepage.api;
 
+import com.ryan9025.myhomepage.dto.CustomUserDetails;
+import com.ryan9025.myhomepage.dto.SubscribeDto;
 import com.ryan9025.myhomepage.entity.Member;
 import com.ryan9025.myhomepage.service.MemberService;
+import com.ryan9025.myhomepage.service.SubscribeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberApiController {
     private final MemberService memberService;
+    private final SubscribeService subscribeService;
 
-    @PutMapping("/api/member/{id}/profileImageUrl")
+    @PutMapping("/member/{id}/profileImageUrl")
     public Map<String,Object> profileImageUpdate(@PathVariable int id,
                                                  MultipartFile profileImageUrl) {
         log.info("profileImageUrl=={}",profileImageUrl);
@@ -27,6 +32,17 @@ public class MemberApiController {
         resultMap.put("isState","OK");
         //이미지도 내려주기!
         resultMap.put("memberInfo",memberInfo); // jackson object를 json으로 리턴
+        return resultMap;
+    }
+
+    @GetMapping("/member/{urlID}/subscribe")
+    public Map<String, Object> subscribeList(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable int urlID) {
+        List<SubscribeDto> subscribeList = subscribeService.subscribeList(
+                customUserDetails.getLoggedMember().getId(),urlID);
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("subscribeList",subscribeList);
         return resultMap;
     }
 }
--- src/main/java/com/ryan9025/myhomepage/service/SubscribeService.java
+++ src/main/java/com/ryan9025/myhomepage/service/SubscribeService.java
@@ -1,16 +1,23 @@
 package com.ryan9025.myhomepage.service;
 
+import com.ryan9025.myhomepage.dto.SubscribeDto;
 import com.ryan9025.myhomepage.repository.SubscribeRepository;
 import groovy.util.logging.Slf4j;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class SubscribeService {
     private final SubscribeRepository subscribeRepository;
+    private final EntityManager entityManager;
     @Transactional
     public void subscribe(int customerDetailsID, int urlID) {
         subscribeRepository.subscribe(customerDetailsID, urlID);
@@ -24,6 +31,24 @@ public class SubscribeService {
         return subscribeRepository.subscribeCount(memberID);
     }
 
-
-
+    //구독 리스트 만들기
+    @Transactional
+    public List<SubscribeDto> subscribeList(int customUserDetailsID, int urlID) {
+        String queryString =
+                "SELECT b.MEMBER_ID AS ID , b.PROFILEIMAGEURL , b.NAME, " +
+                "NVL2((SELECT 1 FROM SUBSCRIBE WHERE FROMMEMBERID = ? AND TOMEMBERID = b.MEMBER_ID),TO_CHAR(1),TO_CHAR(0)) " +
+                "AS subscribeState, " +
+                "NVL2((SELECT 1 FROM dual WHERE ? = b.MEMBER_ID),TO_CHAR(1),TO_CHAR(0)) AS equalState " +
+                "FROM BLOG_MEMBER b INNER JOIN SUBSCRIBE s " +
+                "ON b.MEMBER_ID = s.TOMEMBERID " +
+                "WHERE s.FROMMEMBERID = ?";
+        Query query = entityManager.createNativeQuery(queryString)
+                .setParameter(1,customUserDetailsID)
+                .setParameter(2,customUserDetailsID)
+                .setParameter(3,urlID);
+        //QLRM
+        JpaResultMapper jpaResultMapper = new JpaResultMapper();
+        List<SubscribeDto> subscribeList = jpaResultMapper.list(query,SubscribeDto.class);
+        return subscribeList;
+    }
 }
