b52819441e062f56d85e5a5ba81f0568d53338c5
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-15 17:15:54 +0900

    feat: story -> feed로 변경

--- src/main/java/com/ryan9025/myhomepage/controller/ImageController.java
+++ src/main/java/com/ryan9025/myhomepage/controller/ImageController.java
@@ -1,18 +1,31 @@
 package com.ryan9025.myhomepage.controller;
 
+import com.ryan9025.myhomepage.dto.CustomUserDetails;
+import com.ryan9025.myhomepage.dto.ImageUploadDto;
+import com.ryan9025.myhomepage.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/image")
+@RequiredArgsConstructor
 public class ImageController {
-    @GetMapping("/story")
-    public String story() {
-        return "/image/story";
+    private final ImageService imageService;
+    @GetMapping("/feed")
+    public String feed() {
+        return "/image/feed";
     }
     @GetMapping("/upload")
     public String upload() {
         return "/image/upload";
+    }
+    @PostMapping("/upload")
+    public String imageUpload(ImageUploadDto imageUploadDto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        imageService.upload(imageUploadDto,customUserDetails);
+        return "redirect:/member/myPage/" + customUserDetails.getLoggedMember().getId();
     }
 }
