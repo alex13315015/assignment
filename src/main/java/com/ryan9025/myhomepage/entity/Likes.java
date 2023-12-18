8612258d1013f3907eda1f3abf5ce3ea04450d3b
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-18 09:36:08 +0900

    feat: Likes 테이블 생성

---
+++ src/main/java/com/ryan9025/myhomepage/entity/Likes.java
@@ -0,0 +1,37 @@
+package com.ryan9025.myhomepage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class) // 자동으로 날짜를 생성
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "imageID")
+    private Image image;
+
+    @ManyToOne
+    @JoinColumn(name = "memberID")
+    private Member member;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
