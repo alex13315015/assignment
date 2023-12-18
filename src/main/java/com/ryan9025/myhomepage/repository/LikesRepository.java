a6cbb402684cc573a14e9e45d79bc5315459b50a
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-18 09:36:25 +0900

    feat: LikesRepository 생성

---
+++ src/main/java/com/ryan9025/myhomepage/repository/LikesRepository.java
@@ -0,0 +1,8 @@
+package com.ryan9025.myhomepage.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikesRepository extends JpaRepository {
+}
