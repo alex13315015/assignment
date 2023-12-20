920184f2384feb72ba4cbbf204f7d05dd2acfea9
alex13315015 <150584133+alex13315015@users.noreply.github.com>
2023-12-20 15:19:03 +0900

    feat: EntityListeners 어노테이션 추가

--- src/main/java/com/ryan9025/myhomepage/entity/Comments.java
+++ src/main/java/com/ryan9025/myhomepage/entity/Comments.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 @ToString
 @Entity
+@EntityListeners(AuditingEntityListener.class) // 자동으로 날짜를 생성
 public class Comments {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
